fbb814e25be23c8e39014e230b542a18f2c4d27c
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-21 18:29:55 -0700

    adding jquery classes html css and js

---
+++ js/colors-scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
+  });
+})
